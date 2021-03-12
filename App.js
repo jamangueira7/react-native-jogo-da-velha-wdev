a875824b7ca5e48c7572a1c4771c0766c65313d5
João Mangueira <jamjs7@gmail.com>
2021-03-12 11:55:17 -0300

    Pagina de menu pronta e states criados

--- App.js
+++ App.js
@@ -1,14 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [tela, setTela] = useState('menu');
+  const [jogadorAtual, setJogadorAtual] = useState('');
+  const [tabuleiro, setTabuleiro] = useState([]);
+  const [jogadasRestantes, setJogadasRestantes] = useState(0);
+  const [ganhador, setGanhador] = useState('');
+
+  switch (tela) {
+    case 'menu':
+      return getTelaMenu();
+    case 'jogo':
+      return getTelaJogo();
+    case 'ganhador':
+      return getTelaGanhador();
+  }
+
+  function getTelaMenu() {
+    return (
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+          <Text style={styles.titulo}>Jogo da velha</Text>
+          <Text style={styles.subtitulo}>Selecione o primeiro jogador</Text>
+
+          <View style={styles.inlineItems}>
+            <TouchableOpacity style={styles.boxJogador}>
+              <Text style={styles.jogadorX}>X</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.boxJogador}>
+              <Text style={styles.jogadorO}>O</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+    );
+  }
+
+  function getTelaJogo() {
+    return (
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+          <Text>Jogo</Text>
+        </View>
+    );
+  }
+
+  function getTelaGanhador() {
+    return (
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+          <Text>Ganhador</Text>
+        </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +64,40 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  titulo: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+
+  subtitulo: {
+    fontSize: 20,
+    color: '#555',
+    marginTop: 20,
+  },
+
+  inlineItems: {
+    flexDirection: 'row',
+  },
+
+  boxJogador: {
+    width: 80,
+    height: 80,
+    backgroundColor: "#ddd",
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 5,
+  },
+
+  jogadorX: {
+    fontSize: 40,
+    color: '#553fda',
+  },
+
+  jogadorO: {
+    fontSize: 40,
+    color: '#da3f3f',
   },
 });
